c08efcf6ed7b11dc043c981476b94b3f94ab10a2
racedevgms <racedev@gmsracecars.com>
2026-05-16 12:44:09 -0400

    add error handling to submission form

--- src/main.py
+++ src/main.py
@@ -5,15 +5,22 @@ def handler(event, context):
     body = json.loads(event['body'])
 
     if body['type'] == 'interest form submission':
-        InterestFormSubmissionHandler(body)
+        return InterestFormSubmissionHandler(body)
 
 def InterestFormSubmissionHandler(body):
-    form = body['data']
-    print("Form data received:")
-    print(form)
-    # Process the interest form submission
-    
-    return {
-        'statusCode': 200,
-        'body': json.dumps({'message': 'Interest form submitted successfully'})
-    }
+    try:
+        form = body['data']
+        print("Form data received:")
+        print(form)
+        # Process the interest form submission
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps({'message': 'Interest form submitted successfully'})
+        }
+    except Exception as e:
+        print(f"Error processing interest form: {e}")
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'message': 'Failed to process interest form submission'})
+        }
